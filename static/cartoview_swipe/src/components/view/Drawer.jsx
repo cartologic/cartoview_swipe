e091c18da581bf0ef51084ed3d16ec0146aaedda
Khaled Boka <bokae97@gmail.com>
2018-04-16 14:18:44 +0200

    fixed: navigation tools has no effect | Except the zoom controller

--- static/cartoview_swipe/src/components/view/Drawer.jsx
+++ static/cartoview_swipe/src/components/view/Drawer.jsx
@@ -92,18 +92,27 @@ class CartoviewDrawer extends React.Component {
                             </ListItemIcon>
                             <ListItemText primary="Export Map" />
                         </ListItem>
-                        <CollapsibleListItem open={false} title="Layers" icon={<LayersIcons />} >
-                            <CartoviewLayerSwitcher handleLayerVisibilty={handleLayerVisibilty} changeLayerOrder={changeLayerOrder} mapLayers={mapLayers} />
-                        </CollapsibleListItem>
-                        <CollapsibleListItem open={false} title="Basemaps" icon={<LayersIcons />} >
-                            <CartoviewBasemapSwitcher 
-                                visibleBasemapName={visibleBasemapName} 
-                                handleLayerVisibilty={handleBasemapVisibilty} 
-                                mapLayers={basemapLayers} />
-                        </CollapsibleListItem>
-                        <CollapsibleListItem open={false} title="Legend" icon={<ImageIcon />} >
-                            <CartoviewLegends legends={legends} />
-                        </CollapsibleListItem>
+                        {
+                            config.showLayerSwitcher && 
+                            <CollapsibleListItem open={false} title="Layers" icon={<LayersIcons />} >
+                                <CartoviewLayerSwitcher handleLayerVisibilty={handleLayerVisibilty} changeLayerOrder={changeLayerOrder} mapLayers={mapLayers} />
+                            </CollapsibleListItem>
+                        }
+                       {
+                           config.showBaseMapSwitcher && 
+                            <CollapsibleListItem open={false} title="Basemaps" icon={<LayersIcons />} >
+                                <CartoviewBasemapSwitcher 
+                                    visibleBasemapName={visibleBasemapName} 
+                                    handleLayerVisibilty={handleBasemapVisibilty} 
+                                    mapLayers={basemapLayers} />
+                            </CollapsibleListItem>
+                       }
+                       {
+                           config.showLegend &&
+                            <CollapsibleListItem open={false} title="Legend" icon={<ImageIcon />} >
+                                <CartoviewLegends legends={legends} />
+                            </CollapsibleListItem>
+                       }
                         <CartoviewAbout open={about} title={config.formTitle} abstract={config.formAbstract} close={this.handleAboutChange} />
                     </List>
                 </Paper>
